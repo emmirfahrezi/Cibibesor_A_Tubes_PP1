ffaea9a4a1032abf97f10c8a98e6ce5df1347a43
emmirfahrezi <emmir.fahrezi1@gmail.com>
2025-05-27 17:09:43 +0700

    pindah ke branch dev

---
+++ src/service/halo.java
@@ -0,0 +1,10 @@
+package service;
+
+public class halo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It prints messages to the console.");
+        System.out.println("Feel free to modify and expand it!");
+    }
+}
