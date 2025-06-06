d24406271e0987735948eb2f422dc1c2b35156d8
phikmat9 <phikmat9@gmail.com>
2025-06-06 20:15:23 +0700

    feat: update produk

--- src/tubes/StrukturProduk.java
+++ src/tubes/StrukturProduk.java
@@ -8,4 +8,21 @@ public class StrukturProduk {
     public boolean isEmpty() {
     return HEAD == null;
   }
+
+  // hikmat pandu raharja
+  // fitur update produk
+  public void updateProduk(int posisi, Produk dataBaru) {
+    Node curNode = HEAD;
+    int index = 1;
+    while (curNode != null && index < posisi) {
+      curNode = curNode.getNext();
+      index++;
+    }
+    if (curNode != null) {
+      curNode.setData(dataBaru);
+      System.out.println("Produk di posisi " + posisi + " berhasil diperbarui.");
+    } else {
+      System.out.println("Posisi tidak ditemukan.");
+    }
+  }
 }
