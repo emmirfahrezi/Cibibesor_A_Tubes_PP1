8ad8706d4ede00846a2218805d33d55384b8a8ed
emmirfahrezi <emmir.fahrezi1@gmail.com>
2025-06-06 20:20:24 +0700

    feat: tambah fitur tambahTail dan tambahHead

--- src/tubes/StrukturProduk.java
+++ src/tubes/StrukturProduk.java
@@ -3,10 +3,35 @@ package tubes;
 import java.io.*;
 
 public class StrukturProduk {
-    public Node HEAD;
+  public Node HEAD;
 
-    public boolean isEmpty() {
+  public boolean isEmpty() {
     return HEAD == null;
+  }
+
+  // Emmir Fahrezi
+  // fitur tambah produk di awal
+   public void tambahHead(Produk data) {
+    Node newNode = new Node(data);
+    newNode.setNext(HEAD);
+    HEAD = newNode;
+    System.out.println("Produk ditambahkan di awal (HEAD).");
+  }
+
+  // Emmir Fahrezi
+  // fitur tambah produk di akhir
+  public void tambahTail(Produk data) {
+    Node newNode = new Node(data);
+    if (isEmpty()) {
+      HEAD = newNode;
+    } else {
+      Node cur = HEAD;
+      while (cur.getNext() != null) {
+        cur = cur.getNext();
+      }
+      cur.setNext(newNode);
+    }
+    System.out.println("Produk ditambahkan di akhir (TAIL).");
   }
 
   // hikmat pandu raharja
